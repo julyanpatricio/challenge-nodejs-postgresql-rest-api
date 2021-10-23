0d9ef6ca0f5857377c38d71692cdb0ef4e9e1af3
julyanpatricio <julyan.patricio@gmail.com>
2021-10-23 09:29:00 -0300

    add cors config

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyparser = require('body-parser');
-const server = express();
 const routes = require('./routes/index.js');
+const server = express();
 
 server.use(morgan('dev'));
 server.use(express.json())
 server.use(bodyparser.urlencoded({ extended: false }));
 
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 server.use('/', routes);
 
+// Error catching endware.
 server.use((err, req, res, next) => { 
   const status = err.status || 500;
   const message = err.message || err;
-  console.log('############ ERROR #############')
   console.error(err);
   res.status(status).send(message);
 });
